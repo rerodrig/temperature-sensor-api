7a5cd84d52d341486330deb353f4dac4b61cd90e
Renato Rodrigues <renatorodriguesnet@gmail.com>
2019-04-07 12:14:38 -0300

    Fixing mongodb embedded configuration

--- src/test/java/com/mycompany/temperaturesensor/controller/AbstractControllerIT.java
+++ src/test/java/com/mycompany/temperaturesensor/controller/AbstractControllerIT.java
@@ -4,15 +4,12 @@ import com.mycompany.temperaturesensor.repository.entity.TemperatureMeasurementE
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import({ EmbeddedMongoAutoConfiguration.class })
 public abstract class AbstractControllerIT {
 
     @Autowired
